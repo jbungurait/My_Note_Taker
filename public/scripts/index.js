6357562afb54694271545bfb496d94402a2cd6f5
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-03-19 12:40:14 -0700

    fixed script route on html

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -31,7 +31,12 @@ const getNotes = () =>
     headers: {
       'Content-Type': 'application/json',
     },
-  });
+  }.then((response) => response.json())
+  .then((data) => data)
+  .catch((error) => {
+    console.error('Error:', error);
+  })
+  );
 
 const saveNote = (note) =>
   fetch('/api/notes', {
--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('')
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
